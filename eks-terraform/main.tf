76cf7453dfe31707e17f3ada3cb979084557dfbd
ravibhadarge <ravibhadarge15@gmail.com>
2026-08-30 12:08:56 +0530

    eks

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -178,7 +178,7 @@ resource "aws_eks_node_group" "node-grp" {
   subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
   capacity_type   = "ON_DEMAND"
   disk_size       = 20
-  instance_types  = ["t2.large"]
+  instance_types  = ["c7i-flex.large"]
 
   labels = {
     env = "dev"
@@ -189,9 +189,9 @@ resource "aws_eks_node_group" "node-grp" {
   }
 
   scaling_config {
-    desired_size = 3
-    max_size     = 10
-    min_size     = 2
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
   }
 
   update_config {
